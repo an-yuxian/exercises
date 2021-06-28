1565ce00d4b928fe7f50d1657459eefc34b41732
unknown <an-yuxian@outlook.com>
2021-06-28 12:05:26 +0800

    add file

---
+++ amp model.py
@@ -0,0 +1,17 @@
+import math
+g=9.8
+L=5.48
+m=6426
+w=12000/60   
+c=10**4
+
+def sqrt(a):
+    return math.sqrt(a)
+
+def amp(w,k):
+    K=m*g*L+k*L
+    return (sqrt((c**2)*(w**2)+(K-m*w)**2))/((K-m*w)**2+(c**2)*(w**2))
+
+for k in [2,3,4,5,6,7]:
+    print(amp(w,k*10**10))
+
